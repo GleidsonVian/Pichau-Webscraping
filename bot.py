88f3c5182c2a13f47e89c10af702e2269866d078
Gleidson Viana da Cruz <134108223+GleidsonVian@users.noreply.github.com>
2024-05-01 15:43:22 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
 
@@ -45,13 +46,36 @@ def criar_arquivo_excel(nomes, precos, links, nome_do_arquivo_excel):
     df.to_excel(f'{nome_do_arquivo_excel}.xlsx', index=False)
     print(f"Arquivo '{nome_do_arquivo_excel}.xlsx' criado com sucesso!")
 
-def main():
-    item = input('Escolha um item para ser raspado informações: ')
-    url = pegar_url(item)
+def salvar_produto():
+    produto = entrada_produto.get()  # Obtém o texto digitado pelo usuário
+    print("Produto inserido pelo usuário:", produto)
+    # Aqui você pode fazer o que quiser com o produto, como salvá-lo em uma variável
+
+    # Aqui vou chamar a função para raspar os dados
+    url = pegar_url(produto)
     nomes, precos, links = raspar_dados(url)
-    nome_do_arquivo_excel = input('Qual o nome para o arquivo excel atual? ')
-    criar_arquivo_excel(nomes, precos, links,nome_do_arquivo_excel)
+    nome_do_arquivo_excel = produto
+    criar_arquivo_excel(nomes, precos, links, nome_do_arquivo_excel)
 
-if __name__ == "__main__":
-    main()
+def sair():
+    if messagebox.askokcancel("Sair", "Deseja realmente sair?"):
+        janela.destroy()
 
+# Cria a janela principal
+janela = tk.Tk()
+
+# Cria o primeiro Label e a entrada para o produto
+tk.Label(janela, text="Produto:").grid(column=0, row=0)
+entrada_produto = tk.Entry(janela)
+entrada_produto.grid(column=1, row=0)
+
+# Cria o botão para salvar o produto
+botao_produto = tk.Button(janela, text="Raspar Produto", command=salvar_produto)
+botao_produto.grid(column=2, row=0)
+
+# Cria o botão para sair
+botao_sair = tk.Button(janela, text="Sair", command=sair)
+botao_sair.grid(column=0, row=1, columnspan=3, pady=10)
+
+# Inicia o loop principal
+janela.mainloop()
